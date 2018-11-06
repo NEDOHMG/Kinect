1244352655e4010cc2e79ad0361d755b42f0a05c
Antonio Vega <a.vegaramirez76@gmail.com>
2018-11-06 11:35:39 +0900

    Wall

---
+++ Wall/Assets/Scripts/ImageViewer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageViewer : MonoBehaviour {
+
+    // Provide the color and depth data from Kinect
+    public MultiSourceManager mMultiSource;
+
+    // To put the output of the kinect
+    public RawImage mRawImage;
+
+	void Update ()
+    {
+        // Get the color data and display it in the canvas
+        mRawImage.texture = mMultiSource.GetColorTexture();	
+	}
+}
