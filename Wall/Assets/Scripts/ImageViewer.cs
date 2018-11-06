6f2d0d4a56fcbb2e63abb4cd559456d7a6f9f7c1
Antonio Vega <a.vegaramirez76@gmail.com>
2018-11-06 18:43:02 +0900

    Point_Cloud

--- Wall/Assets/Scripts/ImageViewer.cs
+++ Wall/Assets/Scripts/ImageViewer.cs
@@ -5,15 +5,22 @@ using UnityEngine.UI;
 
 public class ImageViewer : MonoBehaviour {
 
+    // Reference for the texture
+    public MeasureDepth mMeasureDepth;
+
     // Provide the color and depth data from Kinect
     public MultiSourceManager mMultiSource;
 
     // To put the output of the kinect
     public RawImage mRawImage;
+    public RawImage mRawDepth;
 
-	void Update ()
+    void Update ()
     {
         // Get the color data and display it in the canvas
-        mRawImage.texture = mMultiSource.GetColorTexture();	
+        mRawImage.texture = mMultiSource.GetColorTexture();
+        // Here we link the texture with the Viewer
+        mRawDepth.texture = mMeasureDepth.mDepthTexture;
+        	
 	}
 }
