9bf0776206a91b5d82915ae93a364b94487405da
Antonio Vega <a.vegaramirez76@gmail.com>
2018-11-07 19:50:35 +0900

    AngleCalculation Class Vector3 Joint

---
+++ Interface_Beta/Assets/Scripts/AnglesCalculation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+
+
+public class AnglesCalculation : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public double AngleBetweenTwoVectors(Vector3 vectorA, Vector3 vectorB)
+    {
+        double dotProduct = 0.0;
+        vectorA.Normalize();
+        vectorB.Normalize();
+        dotProduct = Vector3.Dot(vectorA, vectorB);
+
+        return (double)Math.Acos(dotProduct) / Math.PI * 180;
+    }
+}
