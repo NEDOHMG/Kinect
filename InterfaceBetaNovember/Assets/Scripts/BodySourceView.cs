2acd3ad49babdf8520a075b164c97a172929128f
Ramin <rtadayon@hiroshima-u.ac.jp>
2018-11-26 13:28:38 +0900

    Updated BodySourceView to output results to txt file for neural network

--- InterfaceBetaNovember/Assets/Scripts/BodySourceView.cs
+++ InterfaceBetaNovember/Assets/Scripts/BodySourceView.cs
@@ -5,6 +5,8 @@ using Kinect = Windows.Kinect;
 using System;
 using System.Linq;
 using UnityEngine.UI;
+using UnityEditor;
+using System.IO;
 
 //test
 
@@ -122,6 +124,11 @@ public class BodySourceView : MonoBehaviour
     public Vector3 _RightArmAvatar;
     public Vector3 _RightHandAvatar;
 
+    //Variables for output to log file
+    public static string preamble = "";
+    public static string filename = "";
+    public static string logfile = "";
+
     #endregion
 
     #region Dictionary Joints
@@ -214,6 +221,34 @@ public class BodySourceView : MonoBehaviour
         _differencesSpinY = new List<double>();
         _calspineList = new List<double>();
 
+        //initialize log file name based on current date and time
+        DateTime dt = DateTime.Now;
+        preamble = "Assets/Resources/";
+        filename = "log_" + dt.ToString("yyyy-MM-dd_HH-mm-ss");
+        logfile = preamble + filename + ".txt";
+        //WriteLog("Test");
+
+    }
+
+    //write an entry to the log file
+    static void WriteLog(string entry)
+    {
+        if (!File.Exists(logfile))
+        {
+            StreamWriter testing = File.CreateText(logfile);
+            testing.Close();
+        }
+        //Write some text to the test.txt file
+        StreamWriter writer = File.AppendText(logfile);
+        writer.WriteLine(entry);
+        writer.Close();
+
+        //Re-import the file to update the reference in the editor
+        AssetDatabase.ImportAsset(logfile);
+        TextAsset asset = Resources.Load(filename) as TextAsset;
+
+        //Print the text from the file
+        //Debug.Log(asset.text);
     }
 
     void Update()
@@ -543,7 +578,8 @@ public class BodySourceView : MonoBehaviour
             colorActuator = 100;
 
             //upload current values to neural network here
-        }
+            WriteLog(LeftKneeFlexionAngle + " " + LeftKneeExtensionAngle + " " + RightKneeFlexionAngle + " " + RightKneeExtensionAngle + " " + LeftHipFlexionAngle + " " + LeftHipExtensionAngle + " " + RightHipFlexionAngle + " " + RightHipExtensionAngle + " " + LeftKneeAverageDelta + " " + RightKneeAverageDelta + " " + SpineBaseAverageDelta + " " + FlexionTime + " " + ExtensionTime);
+        } 
 
         if (!Calibrated)
         {
