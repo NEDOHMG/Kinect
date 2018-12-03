4321a21e75f64ebf0b4979075220a279100228dd
Antonio Vega <a.vegaramirez76@gmail.com>
2018-12-03 15:44:49 +0900

    Stable version

---
+++ BluetoothController/BLE/src/main.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include "BluetoothSerial.h"
+ 
+BluetoothSerial SerialBT;
+
+int ledPin = 15;
+boolean LED = false;
+int w = 0;
+
+ 
+void setup() {
+  
+  Serial.begin(115200);
+ 
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  SerialBT.begin("ESP32");
+
+}
+ 
+void loop() {
+ 
+  SerialBT.read();
+  w = SerialBT.parseInt();
+
+  if( w == 1) {
+    LED = false;
+  } else if (w == 0){
+    LED = true;
+  }
+
+  if(LED)
+    {
+      digitalWrite(ledPin, HIGH);
+    } else {
+      digitalWrite(ledPin, LOW);
+    }
+  
+  // clean the variable 
+  w = 0;
+
+}
